1c2141bf62c64863151aa6b46326de3c29b8e1e9
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2023-11-15 23:37:46 +0100

    add order payment

--- src/main/java/com/eclipse/UirShop/transformer/PaymentTransformer.java
+++ src/main/java/com/eclipse/UirShop/transformer/PaymentTransformer.java
@@ -1,4 +1,27 @@
 package com.eclipse.UirShop.transformer;
 
+import com.eclipse.UirShop.entities.Payment;
+import com.eclipse.UirShop.entitiesDto.PaymentDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PaymentTransformer {
+    public static PaymentDto convertPaymentToDto(Payment payment) {
+        return new PaymentDto(
+                payment.getId(),
+                payment.getFirstName(),
+                payment.getLastName(),
+                payment.getModePayment(),
+                payment.getNumberCard(),
+                payment.getExpirationDate(),
+                payment.getSecurityCode()
+        );
+    }
+
+    public static List<PaymentDto> convertPaymentsToDtoList(List<Payment> payments) {
+        return payments.stream()
+                .map(PaymentTransformer::convertPaymentToDto)
+                .collect(Collectors.toList());
+    }
 }
