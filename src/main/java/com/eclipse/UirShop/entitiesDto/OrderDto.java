225a19b2c979fe1d210774187a0a68ab605c7d98
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2023-11-15 23:09:58 +0100

    add order payment

--- src/main/java/com/eclipse/UirShop/entitiesDto/OrderDto.java
+++ src/main/java/com/eclipse/UirShop/entitiesDto/OrderDto.java
@@ -1,4 +1,23 @@
 package com.eclipse.UirShop.entitiesDto;
 
+import com.eclipse.UirShop.enums.StatutOrder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
 public class OrderDto {
+    private @Getter
+    @Setter Long id;
+    private @Getter @Setter Date orderDate;
+    private @Getter @Setter StatutOrder statutOrder;
+    private @Getter @Setter Long totalAmount;
+    private @Getter @Setter PaymentDto payment;
+    public OrderDto(Long id, Date orderDate, StatutOrder statutOrder, Long totalAmount, PaymentDto payment) {
+        this.id = id;
+        this.orderDate = orderDate;
+        this.statutOrder = statutOrder;
+        this.totalAmount = totalAmount;
+        this.payment = payment;
+    }
 }
--- src/main/java/com/eclipse/UirShop/entitiesDto/PaymentDto.java
+++ src/main/java/com/eclipse/UirShop/entitiesDto/PaymentDto.java
@@ -1,4 +1,27 @@
 package com.eclipse.UirShop.entitiesDto;
 
+import com.eclipse.UirShop.enums.ModePayment;
+import lombok.Getter;
+import lombok.Setter;
+
 public class PaymentDto {
+    private @Getter
+    @Setter Long id;
+    private @Getter @Setter String firstName;
+    private @Getter @Setter String lastName;
+    private @Getter @Setter ModePayment modePayment;
+    private @Getter @Setter String numberCard;
+    private @Getter @Setter String expirationDate;
+    private @Getter @Setter String securityCode;
+
+    // Constructeur pour transformer depuis l'entité
+    public PaymentDto(Long id, String firstName, String lastName, ModePayment modePayment, String numberCard, String expirationDate, String securityCode) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.modePayment = modePayment;
+        this.numberCard = numberCard;
+        this.expirationDate = expirationDate;
+        this.securityCode = securityCode;
+    }
 }
