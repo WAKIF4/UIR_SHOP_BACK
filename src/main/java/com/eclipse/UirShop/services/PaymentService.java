28599a82fc46135910e7f626221912bd33e55b19
yassermkh12 <115877348+yassermkh12@users.noreply.github.com>
2023-11-15 23:41:02 +0100

    add order payment

--- src/main/java/com/eclipse/UirShop/services/PaymentService.java
+++ src/main/java/com/eclipse/UirShop/services/PaymentService.java
@@ -1,4 +1,63 @@
 package com.eclipse.UirShop.services;
 
+import com.eclipse.UirShop.entities.Payment;
+import com.eclipse.UirShop.entitiesDto.PaymentDto;
+import com.eclipse.UirShop.repositories.IPaymentRepository;
+import com.eclipse.UirShop.transformer.PaymentTransformer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PaymentService {
+    @Autowired
+    private IPaymentRepository paymentRepository;
+
+    public List<PaymentDto> getAllPayments() {
+        List<Payment> payments = paymentRepository.findAll();
+        return PaymentTransformer.convertPaymentsToDtoList(payments);
+    }
+
+    public PaymentDto getPaymentById(Long paymentId) {
+        Optional<Payment> paymentOptional = paymentRepository.findById(paymentId);
+        return paymentOptional.map(PaymentTransformer::convertPaymentToDto).orElse(null);
+    }
+
+    public PaymentDto createPayment(PaymentDto paymentDto) {
+        Payment payment = new Payment(
+                paymentDto.getFirstName(),
+                paymentDto.getLastName(),
+                paymentDto.getModePayment(),
+                paymentDto.getNumberCard(),
+                paymentDto.getExpirationDate(),
+                paymentDto.getSecurityCode()
+        );
+
+        paymentRepository.save(payment);
+        return PaymentTransformer.convertPaymentToDto(payment);
+    }
+
+    public PaymentDto updatePayment(Long paymentId, PaymentDto updatedPaymentDto) {
+        Optional<Payment> paymentOptional = paymentRepository.findById(paymentId);
+        if (paymentOptional.isPresent()) {
+            Payment existingPayment = paymentOptional.get();
+            existingPayment.setFirstName(updatedPaymentDto.getFirstName());
+            existingPayment.setLastName(updatedPaymentDto.getLastName());
+            existingPayment.setModePayment(updatedPaymentDto.getModePayment());
+            existingPayment.setNumberCard(updatedPaymentDto.getNumberCard());
+            existingPayment.setExpirationDate(updatedPaymentDto.getExpirationDate());
+            existingPayment.setSecurityCode(updatedPaymentDto.getSecurityCode());
+
+            paymentRepository.save(existingPayment);
+            return PaymentTransformer.convertPaymentToDto(existingPayment);
+        } else {
+            return null;
+        }
+    }
+
+    public void deletePayment(Long paymentId) {
+        paymentRepository.deleteById(paymentId);
+    }
 }
