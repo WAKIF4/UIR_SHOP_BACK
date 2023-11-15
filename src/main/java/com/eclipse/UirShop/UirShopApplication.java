899f0cb6cd678183b2dbcc6a2a43023db1310763
Othmane elkhiari <othmaneelkhiari@gmail.com>
2023-11-15 12:40:13 +0100

    Initial commit

---
+++ src/main/java/com/eclipse/UirShop/UirShopApplication.java
@@ -0,0 +1,13 @@
+package com.eclipse.UirShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UirShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UirShopApplication.class, args);
+	}
+
+}
